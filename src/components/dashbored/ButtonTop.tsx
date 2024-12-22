4a83300845e0aae5b786c2e1532078ec16d3cb96
chiheb rahmouni <chihebrahmouni30@gmail.com>
2024-12-22 10:08:43 +0100

    changed landing buttons

--- src/components/dashbored/ButtonTop.tsx
+++ src/components/dashbored/ButtonTop.tsx
@@ -1,49 +1,57 @@
-// import { Link } from "react-router-dom"
-
+// import { Link } from "react-router-dom";
 
 // interface ButtonTopProps {
-//     title: string
-//                 icon:string
-//                 url: string
-//     }
-
-// const ButtonTop = ({title, icon, url}: ButtonTopProps) => {
-//   return (
-//     <Link to={url} className="shadow-hardShadow p-4 flex items-center gap-2 rounded-10">
-//           <img src={icon} className="" alt="Pic" />
-//           <div className="text">
-//               <p className="text-xs text-writingGrey">Vers</p>
-//               <p className="text-lg">{title}</p>
-              
-//           </div>
-//     </Link>
-//   )
+//     title: string;
+//     icon: string;
+//     url: string;
 // }
 
-// export default ButtonTop
+// const ButtonTop = ({ title, icon, url }: ButtonTopProps) => {
+//     return (
+//       <Link
+//         to={url}
+//         className="cardCss flex items-center gap-2 transition-transform duration-300 hover:scale-[102%]"
+//       >
+//         <img
+//           src={icon}
+//           className="w-[30px] h-[30px] object-cover lg:w-[40px] lg:h-[40px]"
+//           alt="Pic"
+//         />
+//         <div className="text">
+//           <p className="text-xs text-writingGrey">Vers</p>
+//           <p className="text-lg">{title}</p>
+//         </div>
+//       </Link>
+//     );
+// };
+
+// export default ButtonTop;
+
+
+
+
+
+
 
 import { Link } from "react-router-dom";
 
 interface ButtonTopProps {
     title: string;
-    icon: string;
-    url: string;
+    Icon: any;
+  url: string;
+  value:number
 }
 
-const ButtonTop = ({ title, icon, url }: ButtonTopProps) => {
+const ButtonTop = ({ title, Icon, url, value }: ButtonTopProps) => {
     return (
       <Link
         to={url}
-        className="cardCss flex items-center gap-2 transition-transform duration-300 hover:scale-[102%]"
+        className="cardCss flex items-center gap-2 transition-transform duration-300 hover:scale-[102%] whitespace-nowrap lg:flex-grow" 
       >
-        <img
-          src={icon}
-          className="w-[30px] h-[30px] object-cover lg:w-[40px] lg:h-[40px]"
-          alt="Pic"
-        />
+        <Icon className="w-[30px] h-[30px] object-cover text-main lg:w-[40px] lg:h-[40px]" />
         <div className="text">
-          <p className="text-xs text-writingGrey">Vers</p>
-          <p className="text-lg">{title}</p>
+          <p className="text-xs text-writingGrey lg:text-base">{title}</p>
+          <p className="text-lg lg:text-2xl">{value} DA</p> 
         </div>
       </Link>
     );
--- src/containers/dashbored/ButtonsCont.tsx
+++ src/containers/dashbored/ButtonsCont.tsx
@@ -1,15 +1,39 @@
+// import ButtonTop from "../../components/dashbored/ButtonTop"
+
+// const ButtonsCont = () => {
+//   return (
+//       <div className="w-full grid grid-cols-2 gap-2 pt-5 md:grid-cols-4 lg:pt-10 lg:gap-6">
+          
+//             {array_buttons.map((item) => (
+//                 <ButtonTop
+//                 key={item.id}
+//                 title={item.title}
+//                 icon={item.icon}
+//                 url={item.url}
+//                 />
+//             ))}
+      
+//     </div>
+//   )
+// }
+import { IoPricetagsOutline } from "react-icons/io5";
+import { CiShoppingCart } from "react-icons/ci";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
+import { TbReportSearch } from "react-icons/tb";
 import ButtonTop from "../../components/dashbored/ButtonTop"
 
 const ButtonsCont = () => {
   return (
-      <div className="w-full grid grid-cols-2 gap-2 pt-5 md:grid-cols-4 lg:pt-10 lg:gap-6">
+      // <div className="w-full grid grid-cols-2 gap-2 pt-5 md:grid-cols-4 lg:pt-10 lg:gap-6">
+      <div className="w-full gap-2 pt-5 flex overflow-auto pb-2 justify-center lg:pt-10 lg:gap-6">
           
             {array_buttons.map((item) => (
                 <ButtonTop
                 key={item.id}
                 title={item.title}
-                icon={item.icon}
+                Icon={item.icon}
                 url={item.url}
+                value={item.value}
                 />
             ))}
       
@@ -19,33 +43,67 @@ const ButtonsCont = () => {
 
 export default ButtonsCont
 
+// const array_buttons = [
+//   {
+//     id: 2,
+//     title: "Produits",
+//     icon: "/menu/cars-for-sale.png",
+//     url: "cars-for-sale",
+//     blank: false,
+//   },
+//   {
+//     id: 3,
+//     title: "Achats",
+//     icon: "/menu/sell-your-car.png",
+//     url: "sell-your-car",
+//     blank: true,
+//   },
+//   {
+//     id: 4,
+//     title: "Ventes",
+//     icon: "/menu/service.png",
+//     url: "service",
+//     blank: false,
+//   },
+//   {
+//     id: 6,
+//     title: "Rapports",
+//     icon: "/menu/blog.png",
+//     url: "blog",
+//     blank: false,
+//   },
+// ];
 const array_buttons = [
   {
     id: 2,
     title: "Produits",
-    icon: "/menu/cars-for-sale.png",
-    url: "cars-for-sale",
+    icon: IoPricetagsOutline,
+    url: "/produits",
     blank: false,
+    value: 120000,
   },
   {
     id: 3,
     title: "Achats",
-    icon: "/menu/sell-your-car.png",
-    url: "sell-your-car",
+    icon: CiShoppingCart,
+    url: "/achats",
     blank: true,
+    value: 12000,
   },
   {
     id: 4,
     title: "Ventes",
-    icon: "/menu/service.png",
-    url: "service",
+    icon: MdOutlineShoppingCartCheckout,
+    url: "/ventes",
     blank: false,
+    value: 876544,
   },
   {
     id: 6,
     title: "Rapports",
-    icon: "/menu/blog.png",
+    icon: TbReportSearch,
     url: "blog",
     blank: false,
+    value: 1234,
   },
 ];
