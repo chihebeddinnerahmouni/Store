fc5cbb75064161d56d09e7b5835d39bdd9db2fae
chiheb rahmouni <chihebrahmouni30@gmail.com>
2024-12-24 18:20:11 +0100

    add achat api

--- src/types/rapport/achats-paiment.ts
+++ src/types/rapport/achats-paiment.ts
@@ -1,16 +1,16 @@
 
 
 
-interface AchatsPaiment {
-    id: number;
-    date: string;
-    montant: number;
-    modePaiement: string;
-    fournisseur: string;
-    reference: string;
-    dateEcheance: string;
-    etat: string;
-    createdAt: string;
-    updatedAt: string;
-}
+// interface AchatsPaiment {
+//     id: number;
+//     date: string;
+//     montant: number;
+//     modePaiement: string;
+//     fournisseur: string;
+//     reference: string;
+//     dateEcheance: string;
+//     etat: string;
+//     createdAt: string;
+//     updatedAt: string;
+// }
     
