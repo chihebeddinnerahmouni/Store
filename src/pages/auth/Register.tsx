a2a1bcdf126d8f68091a03620a61c35b83fa3e66
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-04-15 15:59:07 +0000

    register fix

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,84 +1,81 @@
 import auth_bg from "../../assets/images/auth-bg";
 import logo from "../../assets/images/logo";
-import { useForm, SubmitHandler } from "react-hook-form";
 import InputPassword from "../../components/ui/inputs/InputPassword";
-import { Controller } from "react-hook-form";
-import { useState } from "react";
 import InputEmail from "../../components/ui/inputs/InputEmail";
 import FullShiningButton from "../../components/ui/buttons/FullShiningButton";
 import axios from "axios";
 import { enqueueSnackbar, SnackbarProvider } from "notistack";
 import { useNavigate } from "react-router-dom";
 import InputText from "../../components/ui/inputs/InputText";
-// import InputNumber from "../../components/ui/inputs/InputNumber";
+import { useMutation } from "@tanstack/react-query";
+import { handleAxiosError } from "../../helper/axios_error";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+
 
 type FormValues = {
   email: string;
-    password: string;
-    name: string;
-    surname: string;
-    // phone: string;
-    confirmPassword: string;
+  password: string;
+  name: string;
+  surname: string;
+  // phone: string;
+  confirmPassword: string;
 };
 
+const url = import.meta.env.VITE_BASE_URL as string;
+const sendData = async (values: FormValues) => {
+  const response = await axios.post(`${url}/api/auth/register`, {
+    name: values.name,
+    surname: values.surname,
+    email: values.email,
+    password: values.password,
+    password_confirmation: values.confirmPassword,
+  });
+  return response.data;
+}
+
+
 const Register = () => {
-  const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [name, setName] = useState<string>("");
-    const [surname, setSurname] = useState<string>("");
-    // const [phone, setPhone] = useState<string>("");
-    const [confirmPassword, setConfirmPassword] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const url = import.meta.env.VITE_BASE_URL as string;
+
+
   const navigate = useNavigate();
 
-  // console.log(url);
+  const { mutate, isPending } = useMutation({
+    mutationFn: sendData,
+    onSuccess: (data: any) => {
+      localStorage.setItem("token", data.token);
+      enqueueSnackbar(data.message, { variant: "success" });
+      navigate("/tableau-de-bord");
+    },
+    onError: handleAxiosError
+  });
 
-  const mainColor = "#006233";
 
-  const send = () => {
-    setLoading(true);
-    axios
-      .post(
-        `${url}/api/auth/register`,
-        {
-          name: name,
-          surname: surname,
-          email: email,
-          password: password,
-          password_confirmation: confirmPassword,
-        },
-        {}
-      )
-      .then((res) => {
-        enqueueSnackbar(res.data.message, { variant: "success" });
-        navigate("/tableau-de-bord");
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err.response);
-        setLoading(false);
-        if (err.message === "Network Error") {
-          enqueueSnackbar("Erreur de connexion", { variant: "error" });
-        } else {
-          //   enqueueSnackbar(err.response.data.message, { variant: "error" });
-          Object.keys(err.response.data.details).forEach((key) => {
-            err.response.data.details[key].forEach((e: string) => {
-              enqueueSnackbar(e, { variant: "error" });
-            });
-          });
-        }
-      });
-  };
-
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-      clearErrors,
-        watch
-  } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = send;
+ const formik = useFormik<FormValues>({
+   initialValues: {
+     name: "",
+     surname: "",
+     // phone: "",
+     email: "",
+     password: "",
+     confirmPassword: "",
+   },
+   validationSchema: Yup.object({
+     name: Yup.string().required("Le nom est obligatoire"),
+     surname: Yup.string().required("Le prénom est obligatoire"),
+     email: Yup.string()
+       .email("Email invalide")
+       .required("L'email est obligatoire"),
+     password: Yup.string().required("Le mot de passe est obligatoire"),
+      confirmPassword: Yup.string()
+        .required("Le mot de passe est obligatoire")
+       .oneOf([Yup.ref("password")], "Les mots de passe ne correspondent pas"),
+   }),
+   onSubmit: (values) => {
+     mutate(values);
+   },
+ });
 
   return (
     <SnackbarProvider
@@ -98,53 +95,31 @@ const Register = () => {
             <p className="mt-4 font-bold text-xl">S'inscrire</p>
             <form
               className="mt-4 flex flex-col gap-4 w-full"
-              onSubmit={handleSubmit(onSubmit)}
+              onSubmit={formik.handleSubmit}
             >
               <div className="fields grid grid-cols-2 gap-5">
                 {/* name */}
-                <Controller
-                  name="name"
-                  control={control}
-                  rules={{ required: "Le nom est obligatoire" }}
-                  render={({ field }) => (
-                    <InputText
-                      label="Nom"
-                      {...field}
-                      error={!!errors.name}
-                      helperText={errors.name?.message}
-                      value={name}
-                      setValue={(value: string) => {
-                        setName(value);
-                        field.onChange(value);
-                        if (errors.name) {
-                          clearErrors("name");
-                        }
-                      }}
-                    />
-                  )}
+                <InputText
+                  label="Nom"
+                  error={formik.touched.name && Boolean(formik.errors.name)}
+                  helperText={formik.touched.name && formik.errors.name}
+                  value={formik.values.name}
+                  setValue={(value: string) => {
+                    formik.handleChange("name")(value);
+                  }}
                 />
 
                 {/* surname */}
-                <Controller
-                  name="surname"
-                  control={control}
-                  rules={{ required: "Le prénom est obligatoire" }}
-                  render={({ field }) => (
-                    <InputText
-                      label="Prénom"
-                      {...field}
-                      error={!!errors.surname}
-                      helperText={errors.surname?.message}
-                      value={surname}
-                      setValue={(value: string) => {
-                        setSurname(value);
-                        field.onChange(value);
-                        if (errors.surname) {
-                          clearErrors("surname");
-                        }
-                      }}
-                    />
-                  )}
+                <InputText
+                  label="Prénom"
+                  error={
+                    formik.touched.surname && Boolean(formik.errors.surname)
+                  }
+                  helperText={formik.touched.surname && formik.errors.surname}
+                  value={formik.values.surname}
+                  setValue={(value: string) => {
+                    formik.handleChange("surname")(value);
+                  }}
                 />
 
                 {/* phone */}
@@ -171,91 +146,44 @@ const Register = () => {
                 /> */}
 
                 {/* email */}
-                <Controller
-                  name="email"
-                  control={control}
-                  rules={{
-                    required: "L'email est obligatoire",
-                    pattern: {
-                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                      message: "Veuillez entrer un email valide",
-                    },
+                <InputEmail
+                  label="L'addresse email"
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                  value={formik.values.email}
+                  setValue={(value: string) => {
+                    formik.handleChange("email")(value);
                   }}
-                  render={({ field }) => (
-                    <InputEmail
-                      label="L'addresse email"
-                      {...field}
-                      error={!!errors.email}
-                      helperText={errors.email?.message}
-                      value={email}
-                      setValue={(value: string) => {
-                        setEmail(value);
-                        field.onChange(value);
-                        if (errors.email) {
-                          clearErrors("email");
-                        }
-                      }}
-                    />
-                  )}
                 />
 
                 {/* password */}
-                <Controller
-                  name="password"
-                  control={control}
-                  rules={{ required: "Le mot de pass est obligatoire" }}
-                  render={({ field }) => (
-                    <InputPassword
-                      label="Le mot de passe"
-                      {...field}
-                      error={!!errors.password}
-                      helperText={errors.password?.message}
-                      value={password}
-                      setValue={(value: string) => {
-                        setPassword(value);
-                        field.onChange(value);
-                        if (errors.password) {
-                          clearErrors("password");
-                        }
-                      }}
-                    />
-                  )}
+                <InputPassword
+                  label="Le mot de passe"
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                  value={formik.values.password}
+                  setValue={(value: string) => {
+                    formik.handleChange("password")(value);
+                  }}
                 />
 
                 {/* confirm password */}
-                <Controller
-                  name="confirmPassword"
-                  control={control}
-                  rules={{
-                    required: "Le mot de passe est obligatoire",
-                    validate: (value) =>
-                      value === watch("password") ||
-                      "Les mots de passe ne correspondent pas",
+                <InputPassword
+                  label="Confirmer le mot de passe"
+                  error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                  helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                  value={formik.values.confirmPassword}
+                  setValue={(value: string) => {
+                    formik.handleChange("confirmPassword")(value);
                   }}
-                  render={({ field }) => (
-                    <InputPassword
-                      label="Confirmer le mot de passe"
-                      {...field}
-                      error={!!errors.confirmPassword}
-                      helperText={errors.confirmPassword?.message}
-                      value={confirmPassword}
-                      setValue={(value: string) => {
-                        setConfirmPassword(value);
-                        field.onChange(value);
-                        if (errors.confirmPassword) {
-                          clearErrors("confirmPassword");
-                        }
-                      }}
-                    />
-                  )}
                 />
               </div>
               <FullShiningButton
                 text="S'inscrire"
-                color={mainColor}
-                onClick={handleSubmit(onSubmit)}
                 type="submit"
-                loading={loading}
+                loading={isPending}
               />
             </form>
           </div>
@@ -264,6 +192,5 @@ const Register = () => {
     </SnackbarProvider>
   );
 };
-
 
 export default Register;
